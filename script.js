c60903b9cbee421d093ab17f97ef738ec556566c
Tyler <trcave@gmail.com>
2021-04-21 11:55:57 -0500

    Added read status function and started building a form to add books

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const library = document.querySelector('.library');
+const addBtn = document.querySelector('#add-book');
+const form = document.querySelector('.add-book-form');
 
-const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295 pages', 'not read yet');
-const twilight = new Book('Twilight', 'Stephanie Meyer', '544 pages', 'read');
+
+addBtn.addEventListener('click', buildForm);
+
+const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295 pages', false);
+const twilight = new Book('Twilight', 'Stephanie Meyer', '544 pages', true);
 
 let myLibrary = [theHobbit, twilight];
 
@@ -13,21 +18,27 @@ function Book(title, author, pages, hasBeenRead) {
 }
 
 Book.prototype.info = function() {
-    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.hasBeenRead}`;
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.readStatus()}`;
+}
+
+Book.prototype.readStatus = function() {
+    return (this.hasBeenRead ? 'read' : 'has not read')
 }
 
 function addBookToLibrary() {
     let title = prompt('Title:');
-    let author = prompt('Author:')
-    let pages = prompt('How many pages?')
-    let hasBeenRead = prompt('Have you read this book?')
+    let author = prompt('Author:');
+    let pages = prompt('How many pages?');
+    let hasBeenRead = true;
 
     myLibrary.push(new Book(title, author, pages, hasBeenRead));
-    clearLibrary();
     displayBooks();
 }
 
 function displayBooks() {
+    while (library.firstChild) {
+        library.removeChild(library.firstChild)
+    }
     for (let i=0; i<myLibrary.length; i++) {
         bookCard = document.createElement('div');
         bookCard.classList.add('book');
@@ -36,10 +47,8 @@ function displayBooks() {
     }
 }
 
-function clearLibrary() {
-    while (library.firstChild) {
-        library.removeChild(library.firstChild)
-    }
+function buildForm() {
+    form.innerHTML = '<form><label for="title">Title:</label><input type="text"></input></form>'
 }
 
 displayBooks();
