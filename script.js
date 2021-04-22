a55d2e88f16a606bd139809b016d3d4dc15c4858
Tyler <trcave@gmail.com>
2021-04-22 13:31:02 -0500

    Added new book form

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 const library = document.querySelector('.library');
-const addBtn = document.querySelector('#add-book');
+const addFormBtn = document.querySelector('#add-book-btn');
 const form = document.querySelector('.add-book-form');
+const addBookBtn = document.querySelector('#add');
+const cancelBtn = document.querySelector('#cancel');
 
+const formTitle = document.querySelector('#title');
+const formAuthor = document.querySelector('#author');
+const formPages = document.querySelector('#pages');
+const formRead = document.querySelector('#read-value');
 
-addBtn.addEventListener('click', buildForm);
+addFormBtn.addEventListener('click', openForm);
+addBookBtn.addEventListener('click', addBookToLibrary);
+cancelBtn.addEventListener('click', closeForm);
 
-const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295 pages', false);
-const twilight = new Book('Twilight', 'Stephanie Meyer', '544 pages', true);
+const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295', false);
+const twilight = new Book('Twilight', 'Stephanie Meyer', '544', true);
 
 let myLibrary = [theHobbit, twilight];
 
@@ -26,13 +34,14 @@ Book.prototype.readStatus = function() {
 }
 
 function addBookToLibrary() {
-    let title = prompt('Title:');
-    let author = prompt('Author:');
-    let pages = prompt('How many pages?');
-    let hasBeenRead = true;
+    let title = formTitle.value;
+    let author = formAuthor.value;
+    let pages = formPages.value;
+    let hasBeenRead = formRead.checked;
 
     myLibrary.push(new Book(title, author, pages, hasBeenRead));
     displayBooks();
+    clearInputs();
 }
 
 function displayBooks() {
@@ -47,8 +56,21 @@ function displayBooks() {
     }
 }
 
-function buildForm() {
-    form.innerHTML = '<form><label for="title">Title:</label><input type="text"></input></form>'
+function openForm() {
+    document.getElementById('form-pop-up').style.display = 'block';
+    addFormBtn.style.display = 'none';
+}
+
+function closeForm() {
+    document.getElementById('form-pop-up').style.display = 'none';
+    addFormBtn.style.display = null;
+}
+
+function clearInputs() {
+    formTitle.value = null;
+    formAuthor.value = null;
+    formPages.value = null;
+    formRead.checked = null;
 }
 
 displayBooks();
